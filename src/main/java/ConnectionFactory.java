eccacf2a656e994cdd8e2e2f7cd2368be9185981
Allan Chavier Felintro <allan.felintro@matera.com>
2023-01-30 22:08:09 -0300

    Implementação da ConnectionFactory para comportar pools de conexão com o banco

--- src/main/java/ConnectionFactory.java
+++ src/main/java/ConnectionFactory.java
@@ -5,14 +5,27 @@
  * Data: 30/01/2023
  */
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
+
+    private DataSource dataSource;
+
+    public ConnectionFactory() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/cursos_alura?useTimezone=true&serverTimezone=UTC");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("");
+
+        this.dataSource = comboPooledDataSource;
+    }
+
     public Connection recuperaConexao() throws SQLException {
-        return DriverManager
-            .getConnection("jdbc:mysql://localhost/cursos_alura?useTimezone=true&serverTimezone=UTC","root", "");
+        return this.dataSource.getConnection();
 
     }
 }
