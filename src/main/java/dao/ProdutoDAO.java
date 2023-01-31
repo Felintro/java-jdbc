dd193222a82e860f550ba51bdb86ca629a414324
Allan Chavier Felintro <allan.felintro@matera.com>
2023-01-31 14:58:18 -0300

    Pequena refatoração no método de insert e implementação dos métodos findProduto de ProdutoDAO.java

--- src/main/java/dao/ProdutoDAO.java
+++ src/main/java/dao/ProdutoDAO.java
@@ -6,20 +6,21 @@ package dao;
  */
 
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import model.Produto;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
-@NoArgsConstructor
 public class ProdutoDAO {
 
     private Connection connection;
     private static final String SQL_INSERT_PRODUTO = "INSERT INTO produto (ds_nome, ds_descricao) VALUES (?, ?);";
-    private static final String SQL_SELECT_PRODUTO_BY_ID = "SELECT * FROM produto p WHERE p.id = ?";
+    private static final String SQL_SELECT_PRODUTO_BY_ID = "SELECT * FROM produto p WHERE p.id_produto = ?";
+    private static final String SQL_SELECT_PRODUTO_ALL = "SELECT * FROM produto;";
 
-    public void saveProduto(Produto produto) throws SQLException {
+    public void insertProduto(Produto produto) throws SQLException {
         try(PreparedStatement pstmt = connection.prepareStatement(SQL_INSERT_PRODUTO, Statement.RETURN_GENERATED_KEYS)) {
             pstmt.setString(1, produto.getNome());
             pstmt.setString(2, produto.getDescricao());
@@ -32,6 +33,10 @@ public class ProdutoDAO {
                 }
             }
         }
+
+        System.out.println("Produto salvo com sucesso:");
+        System.out.println(String.format("%d - %s - %s", produto.getId(), produto.getNome(), produto.getDescricao()));
+
     }
 
     public Produto findProdutoById(int idProduto) throws SQLException {
@@ -39,22 +44,36 @@ public class ProdutoDAO {
 
         try(PreparedStatement pstmt = connection.prepareStatement(SQL_SELECT_PRODUTO_BY_ID)) {
             pstmt.setInt(1, idProduto);
-            pstmt.executeQuery();
+            ResultSet rst = pstmt.executeQuery();
 
-            try(ResultSet rst = pstmt.getResultSet()){
-                if(rst.next()){
-                    produto.setId(rst.getInt("id_produto"));
-                    produto.setNome(rst.getString("ds_nome"));
-                    produto.setDescricao(rst.getString("ds_descricao"));
-                }
+            if(rst.next()){
+                produto.setId(rst.getInt("id_produto"));
+                produto.setNome(rst.getString("ds_nome"));
+                produto.setDescricao(rst.getString("ds_descricao"));
             }
         }
-
         return produto;
     }
 
-    
+    public List<Produto> findAllProdutos() throws SQLException {
+        List<Produto> produtos = new ArrayList<>();
 
+        try(PreparedStatement pstmt = connection.prepareStatement(SQL_SELECT_PRODUTO_ALL)) {
+            ResultSet rst = pstmt.executeQuery();
 
+            while(rst.next()) {
+                int idProduto = rst.getInt("id_produto");
+                String nomeProduto = rst.getString("ds_nome");
+                String descricaoProduto = rst.getString("ds_descricao");
+
+                Produto produto = new Produto(idProduto, nomeProduto, descricaoProduto);
+
+                produtos.add(produto);
+            }
+        }
+
+        return produtos;
+
+    }
 
 }
