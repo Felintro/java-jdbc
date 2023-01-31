dcf8e0b3138ce15bcb24d87860ae847ef30930b7
Allan Chavier Felintro <allan.felintro@matera.com>
2023-01-31 15:05:41 -0300

    Implementação inicial de TestaListagemComProduto.java

---
+++ src/main/java/testes/model/TestaListagemComProduto.java
@@ -0,0 +1,23 @@
+package testes.model;
+/*
+ * @author allfel
+ *
+ * Data: 31/01/2023
+ */
+
+import dao.ProdutoDAO;
+import factory.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TestaListagemComProduto {
+
+    public static void main(String[] args) throws SQLException {
+        try(Connection connection = new ConnectionFactory().recuperaConexao()) {
+            ProdutoDAO dao = new ProdutoDAO(connection);
+            System.out.println(dao.findAllProdutos());
+        }
+    }
+
+}
