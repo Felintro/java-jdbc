6955dc20df84198b63eff563b5c1ad1550563e4b
Allan Chavier Felintro <allan.felintro@matera.com>
2023-01-31 11:21:38 -0300

    Teste de criaçaõ da pool de conexão para escalar o uso da aplicação

---
+++ src/main/java/TestaPoolConexoes.java
@@ -0,0 +1,21 @@
+
+/*
+ * @author allfel
+ *
+ * Data: 31/01/2023
+ */
+
+import java.sql.SQLException;
+
+public class TestaPoolConexoes {
+    public static void main(String[] args) throws SQLException {
+        ConnectionFactory connectionFactory= new ConnectionFactory();
+
+        for(int i = 1; i <= 20; i++) {
+            connectionFactory.recuperaConexao();
+            System.out.println("Conexão nº: " + i);
+        }
+
+
+    }
+}
