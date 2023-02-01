ee3588c2dbbbbfaea86fadeecc9f2cd6f6200bd7
Allan Chavier Felintro <allan.felintro@matera.com>
2023-02-01 11:08:32 -0300

    implementação de método para mostrar na tela a lista de produtos separados por categoria

--- src/main/java/testes/model/TestaListagemDeCategorias.java
+++ src/main/java/testes/model/TestaListagemDeCategorias.java
@@ -6,8 +6,10 @@ package testes.model;
  */
 
 import dao.CategoriaDAO;
+import dao.ProdutoDAO;
 import factory.ConnectionFactory;
 import model.Categoria;
+import model.Produto;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -19,10 +21,28 @@ public class TestaListagemDeCategorias {
 
         try(Connection connection = new ConnectionFactory().recuperaConexao()) {
             CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
+
             List<Categoria> listaDeCategorias = categoriaDAO.findAllCategorias();
+            listaDeCategorias.stream()
+                .forEach(
+                    ct -> {
+                        System.out.println(ct.getId() + " - " + ct.getNome());
+
+                        try {
+                            for(Produto produto : new ProdutoDAO(connection).findProdutoByCategoria(ct)) {
+                                System.out.println(ct.getNome() + " - " + produto.getNome());
+                            }
+                        } catch(SQLException throwables) {
+                            throwables.printStackTrace();
+                        }
+
+
+                    }
 
 
 
+
+                );
 
         }
 
