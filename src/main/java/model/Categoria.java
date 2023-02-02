f4543b987eb1e1332f12748b1b671aef2d81dac1
Allan Chavier Felintro <allan.felintro@matera.com>
2023-02-02 10:02:37 -0300

    Implementação do atributo produtos para relacionamento com categoria

--- src/main/java/model/Categoria.java
+++ src/main/java/model/Categoria.java
@@ -2,7 +2,11 @@ package model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  * @author allfel
@@ -12,8 +16,19 @@ import lombok.Setter;
 @Setter
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Categoria {
+
     private Integer id;
     private String nome;
+    private List<Produto> produtos = new ArrayList<>();
 
+    public Categoria(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public void adicionarProduto(Produto produto) {
+        produtos.add(produto);
+    }
 }
