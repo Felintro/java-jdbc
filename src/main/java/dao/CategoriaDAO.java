80d1fbd80e60827e5098c6f8414c68d0b310c7aa
Allan Chavier Felintro <allan.felintro@matera.com>
2023-02-02 10:05:00 -0300

    implementação do método findCategoriaInnerJoinProduto e inclusão do mesmo na classe de listagem de categorias

--- src/main/java/dao/CategoriaDAO.java
+++ src/main/java/dao/CategoriaDAO.java
@@ -6,6 +6,7 @@ package dao;
  */
 
 import model.Categoria;
+import model.Produto;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -20,11 +21,15 @@ public class CategoriaDAO {
     private static final String SQL_INSERT_CATEGORIA = "INSERT INTO produto (ds_nome, ds_descricao) VALUES (?, ?);";
     private static final String SQL_SELECT_CATEGORIA_BY_ID = "SELECT * FROM produto p WHERE p.id_produto = ?";
     private static final String SQL_SELECT_CATEGORIA_ALL = "SELECT * FROM categoria;";
+    private static final String SQL_SELECT_CATEGORIA_JOIN_PRODUTO = " SELECT c.id_categoria c_id_categoria, c.ds_nome c_ds_nome, " +
+        " p.id_produto p_id_produto, p.ds_nome p_ds_nome, " +
+        " p.ds_descricao p_ds_descricao, p.id_categoria p_id_categoria " +
+        " FROM categoria c, produto p " +
+        " WHERE c.id_categoria = p.id_categoria;";
 
     public CategoriaDAO(Connection connection) {
         this.connection = connection;
     }
-
 
     public List<Categoria> findAllCategorias() throws SQLException {
         List<Categoria> categorias = new ArrayList<>();
@@ -33,12 +38,36 @@ public class CategoriaDAO {
             pstmt.execute();
 
             try(ResultSet rst = pstmt.getResultSet()) {
-                while(rst.next()){
+                while(rst.next()) {
                     Categoria categoria = new Categoria(rst.getInt("id_categoria"), rst.getString("ds_nome"));
                     categorias.add(categoria);
                 }
             }
 
+        }
+
+        return categorias;
+
+    }
+
+    public List<Categoria> findCategoriaJoinProduto() throws SQLException {
+        Categoria ultima = null;
+        List<Categoria> categorias = new ArrayList<>();
+
+        try(PreparedStatement pstmt = connection.prepareStatement(SQL_SELECT_CATEGORIA_JOIN_PRODUTO)) {
+            pstmt.execute();
+
+            try(ResultSet rst = pstmt.getResultSet()) {
+                while(rst.next()) {
+                    if(ultima == null || ! ultima.getNome().equals(rst.getString("c_ds_nome"))) {
+                        Categoria categoria = new Categoria(rst.getInt("c_id_categoria"), rst.getString("c_ds_nome"));
+                        ultima = categoria;
+                        categorias.add(categoria);
+                    }
+                    Produto produto = new Produto(rst.getInt("p_id_produto"), rst.getString("p_ds_nome"), rst.getString("p_ds_descricao"));
+                    ultima.adicionarProduto(produto);
+                }
+            }
 
         }
 
--- src/main/java/testes/model/TestaListagemDeCategorias.java
+++ src/main/java/testes/model/TestaListagemDeCategorias.java
@@ -6,7 +6,6 @@ package testes.model;
  */
 
 import dao.CategoriaDAO;
-import dao.ProdutoDAO;
 import factory.ConnectionFactory;
 import model.Categoria;
 import model.Produto;
@@ -22,32 +21,17 @@ public class TestaListagemDeCategorias {
         try(Connection connection = new ConnectionFactory().recuperaConexao()) {
             CategoriaDAO categoriaDAO = new CategoriaDAO(connection);
 
-            List<Categoria> listaDeCategorias = categoriaDAO.findAllCategorias();
+            List<Categoria> listaDeCategorias = categoriaDAO.findCategoriaJoinProduto();
             listaDeCategorias.stream()
                 .forEach(
                     ct -> {
                         System.out.println(ct.getId() + " - " + ct.getNome());
-
-                        try {
-                            for(Produto produto : new ProdutoDAO(connection).findProdutoByCategoria(ct)) {
-                                System.out.println(ct.getNome() + " - " + produto.getNome());
-                            }
-                        } catch(SQLException throwables) {
-                            throwables.printStackTrace();
+                        for(Produto produto : ct.getProdutos()) {
+                            System.out.println(ct.getNome() + " - " + produto.getNome());
                         }
-
-
                     }
-
-
-
-
                 );
-
         }
-
-
-
 
     }
 
